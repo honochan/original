c314ff9a2744088d9848c8db9639f0c4a7a6238d
honochan <scscsic01210@gmail.com>
2020-02-10 01:58:48 +0000

    follow,unfollow

---
+++ db/migrate/20200203032521_create_relationshops.rb
@@ -0,0 +1,11 @@
+class CreateRelationshops < ActiveRecord::Migration[5.2]
+  def change
+    create_table :relationshops do |t|
+      t.references :user, foreign_key: true
+      t.references :follow, foreign_key: { to_table: :users }
+
+      t.timestamps
+      t.index [:user_id, :follow_id], unique: true
+    end
+  end
+end
